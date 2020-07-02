5b1e7996ae76d5333902ad20018ad2acad99b892
Till Kuhn <till.kuhn@dbschenker.com>
2020-07-02 21:40:48 +0200

    cheap sql exporter for places

--- export.js
+++ export.js
@@ -25,6 +25,18 @@ tables.forEach(table => {
     // console.log(items);
     var filename = dumpdir + '/' + table + '.json'
     console.log('Dumping table', table, "to", filename);
+    if (table === 'yummy-place') {
+      var scope='ALL_AUTH';
+      items.forEach( ele => {
+            console.log("INSERT INTO PLACE (name,country,lotype,image_url,coordinates,scope) VALUES('"+ele.name+"','"+ele.country +"','"
+                + ele.lotype+ "','"
+                + ele.imageUrl + "',"
+                + "'{" +ele.coordinates[0] + "," + ele.coordinates[1]+ "}','"
+                +scope+"');");
+          }
+        // console.log("INSERT INTO PLACE (name,country,summary,coordinates,image_url,lotype,scope) VALUES ('some place','de','summary','{4.8234,56.36816}','https://image.url','EXCURS','PUBLIC')");
+      );
+    }
     fs.writeFile(filename, JSON.stringify(items), function (err) {
       if (err) return console.log(err);
       // console.log(items ); // only for debug
